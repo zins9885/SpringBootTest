83627f0450302baae6f5e9ef6ae01fb324370c48
양수진 <soojin9885@naver.com>
2023-09-22 14:20:44 +0900

    ipv4주소 변경

--- SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
+++ SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smhrd.player.service.PlayerService;
 
 @RestController // 리액트 서버로 데이터만 응답
-@CrossOrigin("http://54.180.86.117:3000/") // aws : 서버주소 변경
+@CrossOrigin("http://15.164.171.89:3000/") // aws : 서버주소 변경
 public class PlayerController {
 	
 	@Autowired
